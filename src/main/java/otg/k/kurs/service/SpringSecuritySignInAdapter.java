dca8ab8eb004df706239d10488d8ca527e4a2e2a
gnom7 <system.out.println@mail.ru>
2016-08-05 14:46:16 +0300

    social

--- src/main/java/otg/k/kurs/service/SpringSecuritySignInAdapter.java
+++ src/main/java/otg/k/kurs/service/SpringSecuritySignInAdapter.java
@@ -1,5 +1,6 @@
 package otg.k.kurs.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,6 +10,7 @@ import org.springframework.social.connect.web.SignInAdapter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.NativeWebRequest;
 import otg.k.kurs.domain.Role;
+import otg.k.kurs.repository.UserRepository;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -16,11 +18,15 @@ import java.util.Set;
 @Service
 public class SpringSecuritySignInAdapter implements SignInAdapter {
 
+    @Autowired
+    private UserRepository userRepository;
+
     public String signIn(String localUserId, Connection<?> connection, NativeWebRequest request) {
         Set<GrantedAuthority> authorities = new HashSet<>(1);
         authorities.add(new SimpleGrantedAuthority(Role.ROLE_USER.name()));
+        String password = userRepository.findByUsername(localUserId).getPassword();
         SecurityContextHolder.getContext().setAuthentication(
-                new UsernamePasswordAuthenticationToken(localUserId, null, authorities));
+                new UsernamePasswordAuthenticationToken(localUserId, password, authorities));
         return null;
     }
 }
