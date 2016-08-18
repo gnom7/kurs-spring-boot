a40e974bb7e12703f82f015fbcf51938ca1f5004
gnom7 <system.out.println@mail.ru>
2016-08-18 16:45:51 +0300

    add site in admin panel

--- src/main/java/otg/k/kurs/repository/UserRepository.java
+++ src/main/java/otg/k/kurs/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package otg.k.kurs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import otg.k.kurs.domain.User;
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, String> {
 
-    User findByUsername(String username);
+    User findByUsername(@Param("username") String username);
 
     User findByEmail(String email);
 
